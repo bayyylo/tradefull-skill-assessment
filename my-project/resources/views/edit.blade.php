444fa9c9c9ffcbd37fc8421094e5672d80f14d17
Logan Balas <lbalas@kent.edu>
2021-04-13 16:25:37 -0400

    making style changes to the edit page

--- my-project/resources/views/edit.blade.php
+++ my-project/resources/views/edit.blade.php
@@ -1,12 +1,38 @@
-<form action="/movies/{{ $movie->id }}" method="POST" class="pb-5">
-    @method('PATCH')
-    <div class="form-group">
-        <input type="text" name="title" class="form-control" placeholder="Enter a movie title!" value="{{ old('title') ?? $movie->title }}">
+<style>
+    .row {
+        text-align: center;
+        margin-top: 10%;
+    }
+
+    #save-button {
+        background-color: #088F8F;
+        margin-left: 10px;
+        margin-top: 30px;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 12px;
+    }
+</style>
+
+<div class="row">
+    <div class="col-12">
+        <h2>Editing the title: {{ $movie->title }}</h2>
     </div>
 
-    <button type="submit" class="btn btn-primary">Save Movie</button>
+    <form action="/movies/{{ $movie->id }}" method="POST" class="pb-5">
+        @method('PATCH')
+        <div class="form-group">
+            <input type="text" name="title" size="50" class="form-control" placeholder="Enter a movie title!" value="{{ old('title') ?? $movie->title }}">
+        </div>
 
-    <div>{{ $errors->first('title') }}</div>
+        <button type="submit" id="save-button">Save Movie</button>
 
-    @csrf
-</form>
+        <div>{{ $errors->first('title') }}</div>
+
+        @csrf
+    </form>
+</div>
--- my-project/resources/views/show.blade.php
+++ my-project/resources/views/show.blade.php
@@ -1,19 +1,45 @@
-<div class="row">
-    <div class="col-12">
-        <h1>Details for {{ $movie->title }}</h1>
-    </div>
-</div>
+<style>
+    .row {
+        text-align: center;
+        margin-top: 10%;
+    }
+
+    a {
+        background-color: #088F8F;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+    }
+
+    #delete {
+        background-color: #f44336;
+        margin-left: 10px;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 12px;
+    }
+
+    form {
+        display: inline-block;
+    }
+</style>
 
 <div class="row">
     <div class="col-12">
-        <p><strong>Movie Title: </strong>{{ $movie->title }}</p>
+        <h2>{{ $movie->title }}</h2>
         <a href="{{ $movie->id }}/edit">Edit Movie Title</a>
 
         <form action="{{ $movie->id }}" method="POST">
             @method('DELETE')
             @csrf
 
-            <button type="submit">Delete</button>
+            <button type="submit" id="delete">Delete</button>
         </form>
     </div>
 </div>
